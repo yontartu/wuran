576e1cbeee9651c5da13228cceebcc0f542f11e3
yontartu <jfg47@georgetown.edu>
2018-11-15 16:11:29 -0800

    deleting bash script

--- clean_datafiles.sh
+++
@@ -1,9 +0,0 @@
-for filename in `ls ~/projects/aqi/data/B*` 
-do year=`echo $filename | grep -Eo '_[[:digit:]]{4}'` 
-  file="beijing"$year
-  #file=`echo $
-  echo $file
-  touch $file
-  cut -d, -f1-8,10-11 $filename >> $file
-  mv $file data/$file
-done
